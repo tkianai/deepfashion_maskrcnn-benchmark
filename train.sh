e158525348021965b2bc5651f0242fa329122932
tkianai <tkianai@163.com>
2019-05-30 15:27:43 +0800

    revise the training script

--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python -m torch.distributed.launch --nproc_per_node=8 --nnodes=2 --node_rank=0 --master_addr="10.141.8.84" --master_port=9876 train_net.py --config-file configs/e2e_keypoint_rcnn_R_50_FPN_1x.yaml
+python -m torch.distributed.launch --nproc_per_node=8 --nnodes=$1 --node_rank=0 --master_addr="10.141.8.84" --master_port=9876 train_net.py --config-file configs/e2e_keypoint_rcnn_R_50_FPN_1x.yaml
